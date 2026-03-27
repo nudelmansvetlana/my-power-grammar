e02eff5247a82a0298ee2254dc629a3443fbdc5e
nudelmansvetlana <nudelmansvetlana@gmail.com>
2026-03-27 20:54:52 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Мой сайт grammar-power</h1>
+      <p>Сейчас сюда вставим ваш HTML</p>
+    </div>
+  );
+}
+
+export default App;
